e2ea9d9f008dff1fe4186cbae34f64a2c4d306d7
Yang Ding <502552742@qq.com>
2021-11-14 09:54:16 +0800

    add ARCH&CROSS_PLATFORM defination in Makefile

--- mx6ull_alientek_emmc.sh
+++ mx6ull_alientek_emmc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- distclean
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- mx6ull_14x14_ddr512_emmc_defconfig
-make V=1 ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j4
+make distclean
+make mx6ull_alientek_emmc_defconfig
+make V=1 -j4
